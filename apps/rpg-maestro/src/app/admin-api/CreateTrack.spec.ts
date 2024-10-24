2d403989dbf7ee33f30c445b867667de4e8fd702
fourgate <roman.acevedo62@gmail.com>
2024-10-24 23:20:23 +0200

    feat: check if file is reachable at creation

--- apps/rpg-maestro/src/app/admin-api/CreateTrack.spec.ts
+++ apps/rpg-maestro/src/app/admin-api/CreateTrack.spec.ts
@@ -1,22 +1,41 @@
-import { createTrack } from './CreateTrack';
-import express, { Express } from 'express';
-import * as path from 'node:path';
+import { createTrack } from "./CreateTrack";
+import express, { Express } from "express";
+import * as path from "node:path";
+import http from "http";
 
-it('should return the created track', async () => {
-  const app: Express = express();
-  const port = 3003;
-  app.use(express.static(path.join(__dirname, '../../assets')));
-  app.listen(port, () => {
+let server: http.Server;
+const app: Express = express();
+const port = 3003;
+
+beforeAll(() => {
+  app.use("/public", express.static(path.join(__dirname, "../../assets")));
+  server = app.listen(port, () => {
     console.log(
       `[server]: Server serving static files is running at http://localhost:${port}`
     );
   });
-
-  const res = createTrack({
-    url: `http://localhost:${port}/public/light-switch-sound-198508.mp3`,
-  });
-  expect(res.source.origin_url).toEqual(
-    `http://localhost:${port}/public/light-switch-sound-198508.mp3`
-  );
 });
-// const ress = await fetch(`http://localhost:${port}/public/test.txt`); TODO
+describe("CreateTrack API", () => {
+  it("should return the created track", async () => {
+
+    console.log('create')
+    const res = await createTrack({
+      url: `http://localhost:${port}/public/light-switch-sound-198508.mp3`,
+    });
+    expect(res.source.origin_url).toEqual(
+      `http://localhost:${port}/public/light-switch-sound-198508.mp3`
+    );
+  });
+
+  it("should fail when file is not reachable", async () => {
+    await expect(
+      createTrack({
+        url: `http://localhost:${port}/public/unexisting-file.mp3`,
+      })
+    ).rejects.toThrow("file not reachable");
+  });
+});
+
+afterAll(() => {
+  server.close();
+});
--- apps/rpg-maestro/src/app/admin-api/CreateTrack.ts
+++ apps/rpg-maestro/src/app/admin-api/CreateTrack.ts
@@ -2,8 +2,10 @@ import { TrackCreation } from '../model/TrackCreation';
 import { Track } from '../model/Track';
 import { v4 as uuid } from 'uuid';
 
-export function createTrack(trackCreation: TrackCreation): Track {
+export async function createTrack(trackCreation: TrackCreation): Promise<Track> {
   const now = Date.now();
+  await checkFileIfActuallyUsable(trackCreation.url);
+
   return {
     id: uuid(),
     created_at:now,
@@ -20,4 +22,13 @@ export function createTrack(trackCreation: TrackCreation): Track {
     length: 0,
     tags: trackCreation.tags
   };
+}
+
+async function checkFileIfActuallyUsable(url: string){
+  const response = await fetch(url);
+  if(!response.ok || response.status != 200){
+    const shortError = `httpStatus: ${response.status}, statusText: ${response.statusText}`;
+    console.log(`checkFileIfActuallyUsable failed with error: ${shortError}`, response);
+    throw new Error(`Cannot create track, file not reachable, fetch error: ${shortError}, full error: ${await response.text()}`);
+  }
 }
