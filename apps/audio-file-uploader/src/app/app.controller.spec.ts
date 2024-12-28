90747502fd68394d72db2b591ca623c3abc1d434
fourgate <roman.acevedo62@gmail.com>
2024-12-28 13:08:28 +0100

    feat: move file uploading api into dedicated app audio-file-uploader

---
+++ apps/audio-file-uploader/src/app/app.controller.spec.ts
@@ -0,0 +1,8 @@
+
+describe('AppController', () => {
+  describe('getData', () => {
+    it('should return "Hello API"', () => {
+      expect(1).toEqual(1);
+    });
+  });
+});
--- apps/rpg-maestro/src/app/app.module.ts
+++ apps/rpg-maestro/src/app/app.module.ts
@@ -3,10 +3,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { MulterModule } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
-import { FileUploadService } from './infrastructure/fileUpload/FileUploadService';
-import * as process from 'node:process';
 import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
@@ -17,18 +13,9 @@ import { CacheModule } from '@nestjs/cache-manager';
 
       serveStaticOptions: { redirect: false },
     }),
-    MulterModule.register({
-      storage: diskStorage({
-        destination: `${process.env.FILESERVER_PATH ? process.env.FILESERVER_PATH : '.'}/uploads`,
-        filename: (req, file, cb) => {
-          const filename = `${file.originalname}`;
-          cb(null, filename);
-        },
-      }),
-    }),
     CacheModule.register(),
 ],
   controllers: [AppController],
-  providers: [FileUploadService],
+  providers: [],
 })
 export class AppModule {}
