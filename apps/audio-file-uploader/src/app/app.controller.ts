3ef3efdd6c17797948b24dd200c52ac46c45b52c
fourgate <roman.acevedo62@gmail.com>
2024-12-28 15:40:19 +0100

    feat: add upload from youtube api

--- apps/audio-file-uploader/src/app/app.controller.ts
+++ apps/audio-file-uploader/src/app/app.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express';
 import { FileUploadService } from './fileUpload/FileUploadService';
+import { UploadAudioFromYoutubeRequest } from './fileUpload/fromYoutube/UploadAudioFromYoutubeRequest';
+import { uploadAudioFromYoutube } from './fileUpload/fromYoutube/UploadFromYoutubeService';
 
 @Controller()
 export class AppController {
@@ -12,7 +14,11 @@ export class AppController {
 
   @Post('/upload/audio')
   @UseInterceptors(FileInterceptor('file'))
-  uploadTrack(@UploadedFile() file: Express.Multer.File) {
+  uploadAudio(@UploadedFile() file: Express.Multer.File) {
     return this.fileUploadService.handleFileUpload(file);
+  }
+  @Post('/upload/audio/from-youtube')
+  uploadAudioFromYoutube(@Body() uploadRequest: UploadAudioFromYoutubeRequest) {
+    return uploadAudioFromYoutube(uploadRequest);
   }
 }
---
+++ apps/audio-file-uploader/src/app/fileUpload/fromYoutube/UploadFromYoutubeService.ts
@@ -0,0 +1,68 @@
+import { UploadAudioFromYoutubeRequest } from './UploadAudioFromYoutubeRequest';
+import { UploadAudioFromYoutubeResponse, UploadAudioFromYoutubeResponseForUrl } from './UploadAudioFromYoutubeResponse';
+import { Logger } from '@nestjs/common';
+import ytdl from '@distube/ytdl-core';
+import ffmpeg from 'fluent-ffmpeg';
+import { join } from 'path';
+import { createWriteStream } from 'fs';
+import process from 'node:process';
+
+const UPLOAD_DIRECTORY = `${process.env.FILESERVER_PATH ? process.env.FILESERVER_PATH : '.'}/uploads`;
+
+export async function uploadAudioFromYoutube(
+  uploadRequest: UploadAudioFromYoutubeRequest
+): Promise<UploadAudioFromYoutubeResponse> {
+  let i = 0;
+  const uploadedFilesLinks = [];
+
+  const uploadResult = await Promise.all(
+    uploadRequest.urls.map(async (url) => {
+      if (i > 0) {
+        await sleep(200);
+      }
+      i++;
+      Logger.log(`begin downloading ${url}`);
+      if (!ytdl.validateURL(url)) {
+        return <UploadAudioFromYoutubeResponseForUrl>{ url: url, status: 'error', message: 'Invalid YouTube URL' };
+      }
+
+      try {
+        const info = await ytdl.getInfo(url);
+        const fileName = (info.videoDetails.title ?? `audio_${Date.now()}`).replace(/[^a-zA-Z0-9]/g, '_') + '.mp3';
+        const filePath = join(UPLOAD_DIRECTORY, fileName);
+        const audioStream = ytdl(url, { filter: 'audioonly' });
+
+        await new Promise((resolve, reject) => {
+          const fileStream = createWriteStream(filePath);
+          ffmpeg(audioStream)
+            .audioCodec('libmp3lame')
+            .format('mp3')
+            .on('error', (err) => {
+              Logger.error(`Error converting to MP3 for URL ${url}: ${err.message}`);
+              console.trace(err);
+              fileStream.destroy(err);
+              reject(err);
+            })
+            .on('end', () => {
+              Logger.log(`MP3 saved at: ${filePath}`);
+              resolve(null);
+            })
+            .pipe(fileStream);
+        });
+        uploadedFilesLinks.push(`https://fourgate.cloud/public/musics/uploads/${fileName}`);
+        return <UploadAudioFromYoutubeResponseForUrl>{ url: url, status: 'ok', uploadedFile: fileName };
+      } catch (error) {
+        Logger.error(`Error while downloading audio from URL ${url}: ${error.message}`);
+        console.trace(error);
+        return <UploadAudioFromYoutubeResponseForUrl>{ url: url, status: 'error', message: error.message };
+      }
+    })
+  );
+  return { uploadResult: uploadResult, uploadedFilesLinks: uploadedFilesLinks };
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
