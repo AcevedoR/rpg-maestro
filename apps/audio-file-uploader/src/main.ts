7b88cec444008c0dd1a872e0cd4c7e691005ceb2
fourgate <roman.acevedo62@gmail.com>
2024-12-28 15:40:19 +0100

    feat: add upload from youtube api

--- apps/audio-file-uploader/src/main.ts
+++ apps/audio-file-uploader/src/main.ts
@@ -9,7 +9,8 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   app.enableCors({origin: '*', allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"]});// TODO fix this
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  const server = await app.listen(port);
+  server.setTimeout(3600000);
   Logger.log(`🚀 Application audio-file-uploader is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
