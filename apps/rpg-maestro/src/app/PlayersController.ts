dc8d78bf6704d2591c43404dbcd209e7e61160de
fourgate <roman.acevedo62@gmail.com>
2025-01-01 17:58:42 +0100

    feat(api): split controller and add swagger

---
+++ apps/rpg-maestro/src/app/PlayersController.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Inject, Param } from '@nestjs/common';
+import { TrackService } from './maestro-api/TrackService';
+import { Database } from './maestro-api/Database';
+import { SessionPlayingTracks, Track } from '@rpg-maestro/rpg-maestro-api-contract';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache, Milliseconds } from 'cache-manager';
+import { getConfiguredDatabaseImplementation } from './Configuration';
+
+const ONE_DAY_TTL: Milliseconds = 1000 * 60 * 60 * 24;
+
+@Controller()
+export class PlayersController {
+  private readonly database: Database;
+  private readonly trackService: TrackService;
+
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {
+    this.database = getConfiguredDatabaseImplementation();
+    this.trackService = new TrackService(this.database);
+  }
+
+  @Get('/tracks/:id')
+  getTrack(@Param('id') id: string): Promise<Track> {
+    return this.trackService.get(id);
+  }
+
+  @Get('/sessions/:id/playing-tracks')
+  async getSessionTracks(@Param('id') sessionId: string): Promise<SessionPlayingTracks> {
+    // TODO fix this hack forbidding having more than one instance
+    // this was done to avoid reaching Firestore quotas
+    const cachedValue = (await this.cacheManager.get(sessionId)) as SessionPlayingTracks;
+    if (cachedValue) {
+      return Promise.resolve(cachedValue);
+    }
+    const dbValue = await this.trackService.getSessionPlayingTracks(sessionId);
+    await this.cacheManager.set(sessionId, dbValue, ONE_DAY_TTL);
+    return dbValue;
+  }
+}
