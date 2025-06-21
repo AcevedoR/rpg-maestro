417a5327d7ea3b4de291316425c1e301da29a191
Roman Acevedo <roman.acevedo62@gmail.com>
2025-06-21 17:45:15 +0200

    feat: improve welcome page

--- apps/rpg-maestro-ui/src/app/welcome-page.tsx
+++ apps/rpg-maestro-ui/src/app/welcome-page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
+import GithubSourceCodeLink from './ui-components/github-source-code-link/github-source-code-link';
 
 export function WelcomePage() {
   return (
@@ -9,14 +10,19 @@ export function WelcomePage() {
         padding: '1rem',
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'space-around',
+        justifyContent: 'center',
         alignItems: 'center',
         gap: '1rem',
       }}
     >
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '0' }}>
-        <h1 style={{ margin: 0 }}>Welcome to RPG-MAESTRO</h1>
-        <h4>welcome page todo</h4>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <h1 style={{ margin: 0 }}>Welcome to RPG-MAESTRO!</h1>
+        <h4>A web app to broadcast music to your players during your TTRPG sessions</h4>
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <p>This app is currently in Beta and free to use.</p>
+        <p>Public account creation is coming soon, in the mean time, contact me to have access: <a href="mailto:acevedor.dev@gmail.com">acevedor.dev [at] gmail [dot] com</a></p>
+        <GithubSourceCodeLink />
       </div>
       <ToastContainer limit={5} />
     </div>
