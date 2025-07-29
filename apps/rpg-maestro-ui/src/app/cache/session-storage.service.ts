5d3509643b36005036eb03e68cd9abb7326d81ce
Roman Acevedo <roman.acevedo62@gmail.com>
2025-07-29 18:31:35 +0200

    fix: forgot to include admin as a maestro in ui

--- apps/rpg-maestro-ui/src/app/cache/session-storage.service.ts
+++ apps/rpg-maestro-ui/src/app/cache/session-storage.service.ts
@@ -1,14 +1,17 @@
 import { User } from '@rpg-maestro/rpg-maestro-api-contract';
 
-const USER_SESSION_STORAGE_KEY = 'participants';
+const USER_SESSION_STORAGE_KEY = 'rpg-maestro-users';
 
-export function saveUserInSessionStorage(user: User) {
-  sessionStorage.setItem(USER_SESSION_STORAGE_KEY, JSON.stringify(user));
-}
 export function getUserFromSessionStorage(): User | null {
   const userInSessionStorage = sessionStorage.getItem(USER_SESSION_STORAGE_KEY);
   if (!userInSessionStorage) {
     return null;
   }
   return JSON.parse(userInSessionStorage);
+}
+export function saveUserInSessionStorage(user: User) {
+  sessionStorage.setItem(USER_SESSION_STORAGE_KEY, JSON.stringify(user));
+}
+export function clearUserFromSessionStorage() {
+  sessionStorage.removeItem(USER_SESSION_STORAGE_KEY);
 }
